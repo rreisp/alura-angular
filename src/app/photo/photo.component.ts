cdf59f511fb812a69ae567e4c143c9b50b9d2939
Rodrigo Reis <rreisp@gmail.com>
2019-05-06 14:23:53 -0300

    Aula 02 - Atividade 05 - Inbound properties passando dados para o componente

--- src/app/photo/photo.component.ts
+++ src/app/photo/photo.component.ts
@@ -1,10 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
     selector: 'ap-photo',
     templateUrl: 'photo.component.html'
 })
 export class PhotoComponent {
-    description = 'Leão';
-    url = 'https://img1.topimagens.com/ti/leoes/leoes_001.jpg';
+    @Input() description = '';
+    @Input() url = '';
 }
