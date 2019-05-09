d5b05117b8974b0f784c036a623b8a0f9a7118b2
Rodrigo Reis <rreisp@gmail.com>
2019-05-09 09:48:30 -0300

    Aula 04 - Atividade 05 - BrowserModule e CommonModule

--- src/app/photos/photo/photos.module.ts
+++ src/app/photos/photo/photos.module.ts
@@ -1,11 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
-import { NgModule } from "@angular/core";
 
 import { PhotoComponent } from './photo.component';
+import { PhotoListComponent } from './../photo-list/photo-list.component';
 
 @NgModule({
-    declarations: [ PhotoComponent ],
-    exports: [ PhotoComponent ],
-    imports: [ HttpClientModule ]
+    declarations: [
+        PhotoComponent,
+        PhotoListComponent
+    ],
+    imports: [
+        HttpClientModule,
+        CommonModule
+    ]
 })
 export class PhotosModule { }
