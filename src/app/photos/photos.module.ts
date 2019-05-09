4ec400a56a3446e87db6478033f528eb95189784
Rodrigo Reis <rreisp@gmail.com>
2019-05-09 17:03:36 -0300

    Aula 06 - Atividade 03 - Pipe e implementação

--- src/app/photos/photos.module.ts
+++ src/app/photos/photos.module.ts
@@ -1,3 +1,4 @@
+import { FilterByDescription } from './photo-list/filter-by-description.pipe';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
@@ -7,14 +8,13 @@ import { PhotoListComponent } from './photo-list/photo-list.component';
 import { PhotosComponent } from './photo-list/photos/photos.component';
 import { PhotoComponent } from './photo/photo.component';
 
-
-
 @NgModule({
     declarations: [
         PhotoComponent,
         PhotoListComponent,
         PhotoFormComponent,
-        PhotosComponent
+        PhotosComponent,
+        FilterByDescription
     ],
     imports: [
         HttpClientModule,
