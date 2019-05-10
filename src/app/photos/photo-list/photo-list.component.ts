08e71fc3a3ad8e59768d94f14860ae64ee1672a5
Rodrigo Reis <rreisp@gmail.com>
2019-05-10 10:22:49 -0300

    Aula 07 - Atividade 02 - Submódulos

--- src/app/photos/photo-list/photo-list.component.ts
+++ src/app/photos/photo-list/photo-list.component.ts
@@ -49,5 +49,4 @@ export class PhotoListComponent implements OnInit, OnDestroy {
         }
       });
   }
-
 }
--- src/app/photos/photos.module.ts
+++ src/app/photos/photos.module.ts
@@ -1,26 +1,14 @@
-import { FilterByDescription } from './photo-list/filter-by-description.pipe';
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { HttpClientModule } from '@angular/common/http';
 
-import { PhotoFormComponent } from './photo-form/photo-form.component';
-import { PhotoListComponent } from './photo-list/photo-list.component';
-import { PhotosComponent } from './photo-list/photos/photos.component';
-import { PhotoComponent } from './photo/photo.component';
-import { LoadButtonComponent } from './photo-list/load-button/load-button.component';
+import { PhotoModule } from './photo/photo.module';
+import { PhotoFormModule } from './photo-form/photo-form.module';
+import { PhotoListModule } from './photo-list/photo-list.module';
 
 @NgModule({
-    declarations: [
-        PhotoComponent,
-        PhotoListComponent,
-        PhotoFormComponent,
-        PhotosComponent,
-        FilterByDescription,
-        LoadButtonComponent
-    ],
     imports: [
-        HttpClientModule,
-        CommonModule
+        PhotoModule,
+        PhotoFormModule,
+        PhotoListModule
     ]
 })
 export class PhotosModule { }
